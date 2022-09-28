51f12df2f068cd736735df01e68bf8c430f23303
Buffalodude101 <haydenhart@mail.weber.edu>
2022-09-27 18:35:36 -0600

    sliced string using index

--- python_strings.py
+++ python_strings.py
@@ -24,10 +24,12 @@ current_year = 2022
 #       - second letter of your last name (use the -index)
 #       - first two letter of your first name (use the +index)
 #       - second two letter of your last name (use the -index)
-
-
-
-
+print(first_name)
+print(last_name)
+print(first_name[0])
+print(last_name[-3])
+print(first_name[0 : 2])
+print(last_name[ -2: ])
 #TODO Combining Strings
 #   - Print the following items (one per line) (print using variables)
 #       -first name and last name combined
